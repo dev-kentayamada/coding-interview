14f43bd611cc2e944c850b7572200f10bce53227
dev-kentayamada <55457955+dev-kentayamada@users.noreply.github.com>
2020-07-14 03:55:32 +0000

    Two Sum

--- leetcode/1.two-sum.py
+++ leetcode/1.two-sum.py
@@ -10,25 +10,26 @@ from typing import *
 #
 
 # @lc code=start
-def OnePass_HashTable(nums,target):
-    hash_table = dict()
-    for i, num in enumerate(nums):
-        if target - num in hash_table:
-            return (hash_table[target - num], i)
-        hash_table[num] = i
-    return []
-def TwoPass_HashTable(nums, target):
-    hash_table = dict()
-    for i, num in enumerate(nums):
-        hash_table[num] = i
-    for i, num in enumerate(nums):
-        if target - num in hash_table:
-            if (hash_table[target - num] != i):
+class Answer:
+    def OnePass_HashTable(nums,target):
+        hash_table = dict()
+        for i, num in enumerate(nums):
+            if target - num in hash_table:
                 return (hash_table[target - num], i)
-    return []
+            hash_table[num] = i
+        return []
+    def TwoPass_HashTable(nums, target):
+        hash_table = dict()
+        for i, num in enumerate(nums):
+            hash_table[num] = i
+        for i, num in enumerate(nums):
+            if target - num in hash_table:
+                if (hash_table[target - num] != i):
+                    return (hash_table[target - num], i)
+        return []
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        return TwoPass_HashTable(nums,target)
-        #return OnePass_HashTable(nums,target)
+        return Answer.OnePass_HashTable(nums, target)
+        #return Answer.TwoPass_HashTable(nums, target)
 # @lc code=end
 
