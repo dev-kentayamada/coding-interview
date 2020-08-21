bc6466df6ad98c1d22c3e7a13138fea0811dec8e
dev-kentayamada <55457955+dev-kentayamada@users.noreply.github.com>
2020-08-21 13:34:55 +0000

    Add Doubly Linked List

--- leetcode/707.design-linked-list.py
+++ leetcode/707.design-linked-list.py
@@ -11,7 +11,8 @@ from typing import *
 
 # @lc code=start
 
-
+'''
+# Singly Linked List
 class Node:
     def __init__(self, val):
         self.val = val
@@ -33,16 +34,12 @@ class MyLinkedList:
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
 
-        if (index >= self.size or index < 0):  # list index out of range or index negative
-            return - 1
+        if (index >= self.size):  # list index out of range
+            return -1
 
         temp = self.head
         for _ in range(index):
             temp = temp.next
-
-        # self.printList()  # For debug
-        # print(f"\ndone get {index}\n")  # For debug
-        # print("-----------------------------------") # For debug
         return temp.val
 
     def addAtHead(self, val: int) -> None:
@@ -58,9 +55,6 @@ class MyLinkedList:
             self.head = new_node
 
         self.size += 1
-        # self.printList()  # For debug
-        # print(f"\ndone addAtHead {val}\n")  # For debug
-        # print("-----------------------------------") # For debug
 
     def addAtTail(self, val: int) -> None:
         """
@@ -79,9 +73,6 @@ class MyLinkedList:
             prev.next = new_node
 
         self.size += 1
-        # self.printList()  # For debug
-        # print(f"\ndone addAtTail {val}\n")  # For debug
-        # print("-----------------------------------")
 
     def addAtIndex(self, index: int, val: int) -> None:
         """
@@ -106,9 +97,6 @@ class MyLinkedList:
             new_node.next = temp
 
         self.size += 1
-        # self.printList()  # For debug
-        # print(f"\ndone addAtIndex [{index} {val}]\n")  # For debug
-        # print("-----------------------------------")
 
     def deleteAtIndex(self, index: int) -> None:
         """
@@ -132,9 +120,6 @@ class MyLinkedList:
             temp = None
 
         self.size -= 1
-        # self.printList()  # For debug
-        # print(f"\ndone deleteAtIndex {index}\n")  # For debug
-        # print("-----------------------------------")
 
     # For debug
     def printList(self):
@@ -143,6 +128,122 @@ class MyLinkedList:
             print(temp.val, end=" -> ")
             temp = temp.next
         print("\n")
+'''
+
+
+# Doubly Linked List
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next, self.prev = None, None
+
+
+class MyLinkedList:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+        self.size = 0
+        self.head = None
+
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+
+        if (index >= self.size):  # list index out of range
+            return -1
+
+        cur = self.head
+        for _ in range(index):
+            cur = cur.next
+        return cur.val
+
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        """
+
+        if (self.head == None):
+            self.head = ListNode(val)
+        else:
+            new_node = ListNode(val)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.size += 1
+
+    def addAtTail(self, val: int) -> None:
+        """
+        Append a node of value val to the last element of the linked list.
+        """
+
+        if (self.head == None):
+            self.head = ListNode(val)
+        else:
+            temp = self.head
+            while (temp.next):
+                temp = temp.next
+            new_node = ListNode(val)
+            new_node.prev = temp
+            temp.next = new_node
+        self.size += 1
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        """
+
+        if (index == 0):
+            new_node = ListNode(val)
+            new_node.next = self.head
+            if (self.head != None):
+                self.head.prev = new_node
+            self.head = new_node
+        else:
+            cur = self.head
+            for _ in range(index):
+                prev = cur
+                cur = cur.next
+            new_node = ListNode(val)
+            prev.next = new_node
+            new_node.prev = prev
+            if (cur != None):  # if its NOT None
+                cur.prev = new_node
+            new_node.next = cur
+
+        self.size += 1
+
+    def deleteAtIndex(self, index: int) -> None:
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        """
+
+        if (index >= self.size):  # list index out of range
+            return -1
+
+        if (index == 0):
+            self.head = self.head.next
+        else:
+            cur = self.head
+            for _ in range(index):
+                prev = cur
+                cur = cur.next
+            prior = cur.next
+            prev.next = prior
+            if (prior != None):  # if its NOT None
+                prior.prev = prev
+        self.size -= 1
+
+    # For debug
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.val, end=" -> ")
+            temp = temp.next
+
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
