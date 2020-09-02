7631ad86da5b7b014b316cc20ea523ae46354bd2
dev-kentayamada <55457955+dev-kentayamada@users.noreply.github.com>
2020-09-02 03:11:20 +0000

    Done

---
+++ leetcode/27.remove-element.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        for i in range(len(nums)):
+            if val != nums[i]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+# @lc code=end
